955a4e25b10445cdf5f5e30c42d7b09e40681577
Delilah Carriere <pvt.caboose722@gmail.com>
2016-02-01 14:28:41 -0500

    Task Item Adapter Progress

--- app/src/main/java/ca/delilaheve/scheduler/adapter/ItemListAdapter.java
+++ app/src/main/java/ca/delilaheve/scheduler/adapter/ItemListAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -29,6 +30,18 @@ public class ItemListAdapter extends BaseAdapter {
         View view = inflater.inflate(R.layout.list_item_contact, (ViewGroup) convertView, false);
 
         TextView title = (TextView) view.findViewById(R.id.taskTitle);
+        ImageView editButton = (ImageView) view.findViewById(R.id.editTaskIcon);
+
+        // set title text
+        title.setText("");
+
+        // set edit button click
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // edit task
+            }
+        });
 
         return view;
     }
